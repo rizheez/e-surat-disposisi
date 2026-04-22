e2eca10980ab995f8f294542b8e293672a33d523
rizheez <rezamuhammad980@gmail.com>
2026-04-22 16:40:19 +0800

    feat(provider): enforce HTTPS scheme
    * Added logic to force HTTPS scheme based on request headers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
-use App\Models\SuratMasuk;
 use App\Models\SuratKeluar;
-use App\Observers\SuratMasukObserver;
+use App\Models\SuratMasuk;
 use App\Observers\SuratKeluarObserver;
+use App\Observers\SuratMasukObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +18,14 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (
+            // str_starts_with((string) config('app.url'), 'https://') ||
+            request()->header('x-forwarded-proto') === 'https' ||
+            request()->header('cf-visitor') === '{"scheme":"https"}'
+        ) {
+            URL::forceScheme('https');
+        }
+
         SuratMasuk::observe(SuratMasukObserver::class);
         SuratKeluar::observe(SuratKeluarObserver::class);
     }
