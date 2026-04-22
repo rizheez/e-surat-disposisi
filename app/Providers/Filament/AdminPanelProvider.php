b735d9f932e56c87e675422dfedd7cd9a07db044
rizheez <rezamuhammad980@gmail.com>
2026-04-22 11:41:29 +0800

    feat(admin): enable dark mode for admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->viteTheme('resources/css/filament/admin/theme.css')
+            ->darkMode(false)
             ->login()
             ->brandName('UNU E-Surat')
             ->colors([
